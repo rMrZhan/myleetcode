37bbadd29ef0be6d89d2d20722843cd647d275eb
Administrator <895902052@qq.com>
2020-01-22 13:56:32 +0800

    LC005

--- src/main/java/part1/LC005.java
+++ src/main/java/part1/LC005.java
@@ -11,7 +11,9 @@ public class LC005 {
         int maxStart = 0;  //最长回文串的起点
         int maxEnd = 0;    //最长回文串的终点
         int maxLen = 1;  //最长回文串的长度
+
         boolean[][] dp = new boolean[strLen][strLen];
+
         for (int r = 1; r < strLen; r++) {
             for (int l = 0; l < r; l++) {
                 if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
@@ -23,10 +25,12 @@ public class LC005 {
 
                     }
                 }
+
             }
 
         }
         return s.substring(maxStart, maxEnd + 1);
+
     }
 
 }
