398cdfdb51916dd7484b19a1e93ebda6e9104c13
Administrator <895902052@qq.com>
2020-01-23 19:51:04 +0800

    LC007

--- src/main/java/part1/LC009.java
+++ src/main/java/part1/LC009.java
@@ -1,4 +1,16 @@
 package part1;
 
 public class LC009 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String str = String.valueOf(x);
+        for (int i = 0; i < str.length()/2; i++) {
+            if(str.charAt(i) != str.charAt(str.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
